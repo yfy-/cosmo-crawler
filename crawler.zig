9b1f4b77e668ea0a9007049506e33eca6e0e50e5
Erman Yafay <ermanyafay@gmail.com>
2025-04-24 23:47:24 +0300

    Get page with URL instead of path.

--- crawler.zig
+++ crawler.zig
@@ -12,22 +12,36 @@ pub fn main() !void {
     defer args.deinit();
 
     _ = args.skip();
-    const fname = args.next().?;
+    const url = args.next().?;
 
-    const hfile = try std.fs.cwd().openFile(fname, .{});
-    defer hfile.close();
+    var http_client: std.http.Client = .{ .allocator = allocator };
+    defer http_client.deinit();
 
-    const html_text = try hfile.readToEndAlloc(allocator, 5 * 1024 * 1024);
-    defer allocator.free(html_text);
+    var html_text = std.ArrayList(u8).init(allocator);
+    defer html_text.deinit();
+
+    const header: std.http.Header = .{
+        .name = "User-Agent",
+        .value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) " ++
+            "Gecko/20100101 Firefox/113.0",
+    };
+    const resp = try http_client.fetch(.{
+        .location = .{ .url = url },
+        .method = .GET,
+        .max_append_size = 5 * 1024 * 1024,
+        .response_storage = .{ .dynamic = &html_text },
+        .extra_headers = (&header)[0..1],
+    });
+
+    if (resp.status != std.http.Status.ok) {
+        std.log.err("HTTP Request failed with {}", .{resp.status});
+    }
 
     var stripper = HTMLStripper.init(allocator);
     defer stripper.deinit();
 
-    const html_cont = try stripper.strip(html_text);
+    const html_cont = try stripper.strip(html_text.items);
     defer allocator.free(html_cont);
-    // for (stripper.links.items) |link| {
-    //     std.debug.print("Link: {s}\n", .{link});
-    // }
 
     const stdout = std.io.getStdOut().writer();
     try stdout.writeAll(html_cont);
